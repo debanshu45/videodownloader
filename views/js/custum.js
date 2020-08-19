a213e224594d70edba79fe307a3e61a4a7d74d58
debanshu <debanshu45@gmail.com>
2020-08-19 12:37:56 +0530

    script|css|validation

--- views/js/custum.js
+++ views/js/custum.js
@@ -1,15 +1,46 @@
-var btn = $('#button');
-
-$(window).scroll(function() {
+//scroll to top
+var btn = $("#button");
+$(window).scroll(function () {
   if ($(window).scrollTop() > 100) {
-    btn.addClass('show');
+    btn.addClass("show");
   } else {
-    btn.removeClass('show');
+    btn.removeClass("show");
   }
 });
 
-btn.on('click', function(e) {
+// submit form
+btn.on("click", function (e) {
   e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
+  $("html, body").animate({ scrollTop: 0 }, "300");
 });
 
+var searchVideo = true;
+$(".btn-search").on("click", function (event) {
+  submitTheForm(event);
+});
+function submitTheForm(event) {
+  event.preventDefault();
+  var downloadUrl = document.forms["download-form"]["video"].value;
+  if (downloadUrl == null || downloadUrl == "") {
+   $('.alert-warning').addClass('show');
+  } else {
+    if (searchVideo == true) {
+      $("#loading-message").show();
+      $(".hideOnDownload").hide();
+      $("footer").hide();
+      searchVideo = false;
+
+      setTimeout(function () {
+        $("#download-form")[0].submit();
+      }, 3000);
+
+      setTimeout(function () {
+        searchVideo = true;
+      }, 30000);
+    }
+  }
+}
+
+$('.close').on('click',function(){
+  $(this).parent('.alert').removeClass('show');
+})
